aff4dcc54bf9ce97b84dbd250ddf3ba123a4d05a
Yaroslav Halchenko <debian@onerussian.com>
2022-12-21 10:49:02 -0500

    Keep latest build version in ./latest-version since platform independent
    Windows one becomes a symlink

--- update.sh
+++ update.sh
@@ -7,13 +7,13 @@ src=/mnt/datasets/datalad/ci/git-annex/releases/
 cd $(dirname $0)
 
 latest=$(cd "$src" && /bin/ls -1d *.20* | sort -n | sed -e 's,/,,g' | tail -n 1 )
-current=$(cat windows/dist/build-version)
+current=$(cat latest-version)
 
 if [ $latest != "$current" ]; then
     cp -L "$src/$latest"/*.{deb,changes,dsc,tar.gz} neurodebian/
     cp -L "$src/$latest"/*.dmg osx/
     cp -L "$src/$latest"/*.exe windows/
-    echo "$latest" >| windows/dist/build-version
+    echo "$latest" >| latest-version
     datalad save -m "Updated to $latest from $current" -d^ ./
     datalad push --to=datalad-public
 fi
